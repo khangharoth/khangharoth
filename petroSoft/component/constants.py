bc20586d1547ef3e46d75a3c690b666d8d361fd4
yadvendar007 <yadvendar007@87cfa850-5386-11de-8e0a-c97bf73df9f4>
2010-06-11 16:52:09 +0000

    Adding Constants and Initalize panel added

---
+++ petroSoft/component/constants.py
@@ -0,0 +1,12 @@
+class constants:
+    WELCOMETAB='Welcome'
+    INITIALIZETAB='Initialize'
+
+    USERNAMELABEL='Username:'
+    PWDLABEL='Password:'
+    LOGINLABEL='Login'
+    LOGINFAILEDLABEL='Incorrect Username or Password'
+
+    INITIALIZEWARN='Are you sure you want to initialize whole database'
+    INITIALIZEMESSAGETITLE='Alert'
+    defaultId=-1
