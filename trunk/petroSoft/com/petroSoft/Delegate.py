f433f2493dde557fc2091fd1eada4f392effa5ea
kuldeepsinghk <kuldeepsinghk@87cfa850-5386-11de-8e0a-c97bf73df9f4>
2010-06-13 16:47:32 +0000

    Stock Tab

--- trunk/petroSoft/com/petroSoft/Delegate.py
+++ trunk/petroSoft/com/petroSoft/Delegate.py
@@ -1,7 +1,19 @@
 class Delegate:
+    def __init__(self):
+         self.petrolQunt=0
+         self.dieselQuantity=0
 
     def checkUser(self,userName,pwd):
           if userName=='rashi' and pwd=='rashi':
             return True
           else :
             return  False
+
+    def addToStock(self,petrolQuantity,dieselQuantity):
+
+        self.petrolQunt=self.petrolQunt + int(petrolQuantity)
+        self.dieselQuantity=self.dieselQuantity+int(dieselQuantity)
+
+    def getStock(self):
+        return (self.petrolQunt,self.dieselQuantity)
+
