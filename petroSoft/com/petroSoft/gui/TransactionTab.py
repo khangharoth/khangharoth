4e9ba2451cd8247948f79f55bff7cffd46c580c0
kuldeepsinghk <kuldeepsinghk@87cfa850-5386-11de-8e0a-c97bf73df9f4>
2010-06-13 16:11:14 +0000

    Seperate packages for Module GUI

--- petroSoft/com/petroSoft/gui/TransactionTab.py
+++ petroSoft/com/petroSoft/gui/TransactionTab.py
@@ -1,6 +1,10 @@
+REPORTS = "Reports"
+BANK_DEPOSIT = "Bank Deposits"
+DAILY_TRANSACTION = "Daily Transactions"
+STOCK = "Stock"
+
 import wx
-from com.petroSoft.gui.custom.LabelText import LabelText
-from functools import partial
+
 from com.petroSoft.Constants import constants
 c=constants()
 
@@ -12,10 +16,10 @@ class TransactionTab:
            transactionPanel=wx.Panel(parent,c.defaultId)
            noteBook=wx.Notebook(transactionPanel, c.defaultId, style=(wx.NB_LEFT),size=(800,600))
 
-           self.addTab(noteBook, "Inventory")
-           self.addTab(noteBook,"Daily Transactions")
-           self.addTab(noteBook, "Bank Deposits")
-           self.addTab(noteBook, "Reports")
+           self.addTab(noteBook, STOCK)
+           self.addTab(noteBook, DAILY_TRANSACTION)
+           self.addTab(noteBook, BANK_DEPOSIT)
+           self.addTab(noteBook, REPORTS)
 
 
            return transactionPanel
