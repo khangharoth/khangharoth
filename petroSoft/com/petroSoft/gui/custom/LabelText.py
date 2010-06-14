384099a2315e4d09dd4b3de4f27a73d6130dfc94
kuldeepsinghk <kuldeepsinghk@87cfa850-5386-11de-8e0a-c97bf73df9f4>
2010-06-14 15:57:47 +0000

    Stock Tab

--- petroSoft/com/petroSoft/gui/custom/LabelText.py
+++ petroSoft/com/petroSoft/gui/custom/LabelText.py
@@ -3,6 +3,9 @@ defaultId=-1
 
 class LabelText:
     def __init__(self, label, cord, welcomePanel):
-        self.text_userName = wx.StaticText(welcomePanel, defaultId, label, (cord[0], cord[1]))
-        self.userName = wx.TextCtrl(welcomePanel, defaultId, '', (cord[0] + 100, cord[1]))
+        self.label = wx.StaticText(welcomePanel, defaultId, label, (cord[0], cord[1]))
+        self.text = wx.TextCtrl(welcomePanel, defaultId, '', (cord[0] + 100, cord[1]))
+
+    def getValue(self):
+        return self.text.GetValue()
 
