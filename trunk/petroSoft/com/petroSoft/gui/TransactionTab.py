9e231afc03da451a509a6f420e9693d5ebdcb6be
kuldeepsinghk <kuldeepsinghk@87cfa850-5386-11de-8e0a-c97bf73df9f4>
2010-06-16 15:57:14 +0000

    Stock Tab

--- trunk/petroSoft/com/petroSoft/gui/TransactionTab.py
+++ trunk/petroSoft/com/petroSoft/gui/TransactionTab.py
@@ -6,10 +6,7 @@ STOCK = "Stock"
 import wx
 
 from com.petroSoft.Constants import constants
-from com.petroSoft.Delegate import Delegate
-from com.petroSoft.gui.custom.LabelText import LabelText
-from com.petroSoft.gui.custom.LabelValue import LabelValue
-from functools import partial
+from com.petroSoft.gui.StockPanel import StockPanel
 c=constants()
 
 class TransactionTab:
@@ -17,33 +14,14 @@ class TransactionTab:
            noteBook.AddPage(wx.Panel(noteBook,c.defaultId), name)
 
 
-    def populateCurrentStock(self, stockPanel):
-        currentStock = self.delegate.getStock()
-        stockPanel.currentPetrolStock.text.SetValue(str(currentStock[0]))
-        stockPanel.currentDieselStock.text.SetValue(str(currentStock[0]))
 
-    def addStockTab(self, noteBook):
-        stockPanel = wx.Panel(noteBook, c.defaultId)
-        
-        stockPanel.petrolLabelText = LabelText(c.PETROL, (50, 50), stockPanel,wx.TE_LINEWRAP)
-        stockPanel.dieselLabelText = LabelText(c.Diesel, (50, 150), stockPanel,wx.TE_LINEWRAP)
-        stockPanel.currentPetrolStock = LabelText(c.CURRENT_PETROL_STOCK, (50, 250), stockPanel,wx.TE_READONLY)
-        stockPanel.currentDieselStock = LabelText(c.CURRENT_DIESEL_STOCK,  (50, 300), stockPanel,wx.TE_READONLY)
-
-        submitId = 1
-        stockPanel.submit = wx.Button(stockPanel, submitId, c.SUBMIT, (50, 180))
-        stockPanel.Bind(wx.EVT_BUTTON, partial(self.OnSubmit, stockPanel), id=submitId)
-        stockPanel.countSubmit=0
-        self.populateCurrentStock(stockPanel)
-
-        noteBook.AddPage(stockPanel, STOCK)
 
     def createTransactionTab(self,parent):
            transactionPanel=wx.Panel(parent,c.defaultId)
            noteBook=wx.Notebook(transactionPanel, c.defaultId, style=(wx.NB_LEFT),size=(800,600))
-           self.delegate =Delegate()
+           
 
-           self.addStockTab(noteBook)
+           noteBook.AddPage(StockPanel().createStockPanel(noteBook), STOCK)
 
            self.addTab(noteBook, DAILY_TRANSACTION)
            self.addTab(noteBook, BANK_DEPOSIT)
@@ -52,10 +30,6 @@ class TransactionTab:
 
            return transactionPanel
 
-    def OnSubmit(self,stockPanel,event):
 
-           self.delegate.addToStock(stockPanel.petrolLabelText.getValue(),stockPanel.dieselLabelText.getValue())
-
-           self.populateCurrentStock(stockPanel)
 
     
