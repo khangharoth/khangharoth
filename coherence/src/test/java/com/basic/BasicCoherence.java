b923cdcc0693df9ea671fcdcebce238640a1b017
kuldeepsinghk <kuldeepsinghk@gmail.com>
2012-05-29 01:03:15 +0000

    first coherence checkin

--- coherence/src/test/java/com/basic/BasicCoherence.java
+++ coherence/src/test/java/com/basic/BasicCoherence.java
@@ -3,17 +3,18 @@ package com.basic;
 
 import com.tangosol.net.CacheFactory;
 import com.tangosol.net.NamedCache;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class BasicCoherence {
 
-
-    public static void main(String[] args) {
+    @Test
+    public void shouldDoBasicPutAndGet() {
         NamedCache cache = CacheFactory.getCache("Trade");
 
         cache.put("1", "a");
 
-        System.out.println(cache.get("1"));
-
-
+        Assert.assertEquals(cache.get("1"), "a");
     }
+
 }
