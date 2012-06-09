234cc898529bf29a845562d539144690ce5057da
kuldeepsinghk@gmail.com <kuldeepsinghk@gmail.com@87cfa850-5386-11de-8e0a-c97bf73df9f4>
2012-06-09 01:57:16 +0000

    first coherence checkin

--- trunk/coherence/src/main/java/lesson1/cluster/SpyCoherenceNode.java
+++ trunk/coherence/src/main/java/lesson1/cluster/SpyCoherenceNode.java
@@ -4,30 +4,36 @@ package lesson1.cluster;
 import com.tangosol.coherence.component.util.safeService.safeCacheService.SafeDistributedCacheService;
 import com.tangosol.net.CacheFactory;
 import com.tangosol.net.Cluster;
+import com.tangosol.net.Member;
+import com.tangosol.net.NamedCache;
+import com.tangosol.net.partition.PartitionSet;
 
-import java.util.Enumeration;
+import java.util.Set;
 
 public class SpyCoherenceNode {
 
-    /*
-   Cluster
-   Management
-   DistributedCache
-
-
-    */
+    /*Cluster,Management,DistributedCache */
 
     public static void main(String[] args) throws Exception {
 
         Cluster cluster = CacheFactory.getCluster();
+        NamedCache cache = CacheFactory.getCache("Trade");
 
 
+        distributedServiceInfo(cluster);
+
+    }
+
+    private static void distributedServiceInfo(Cluster cluster) {
         SafeDistributedCacheService service = (SafeDistributedCacheService) cluster.getService("DistributedCache");
+        memberInfo(service);
+    }
 
-        Enumeration serviceNames = cluster.getServiceNames();
-
-        while (serviceNames.hasMoreElements()) {
-            System.out.println(serviceNames.nextElement());
+    private static void memberInfo(SafeDistributedCacheService service) {
+        Set<Member> members = service.getOwnershipEnabledMembers();
+        for (Member member : members) {
+            PartitionSet set = service.getOwnedPartitions(member);
+            System.out.println(member + " having partitions " + set);
         }
     }
 }
