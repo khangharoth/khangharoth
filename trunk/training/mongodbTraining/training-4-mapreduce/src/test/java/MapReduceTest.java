269b35a3fd8debb719d00fec48fb92e650292153
rajbahadur@gmail.com <rajbahadur@gmail.com@87cfa850-5386-11de-8e0a-c97bf73df9f4>
2014-09-03 09:06:05 +0000

    training

---
+++ trunk/training/mongodbTraining/training-4-mapreduce/src/test/java/MapReduceTest.java
@@ -0,0 +1,44 @@
+import com.MongoDBClient;
+import com.mongodb.*;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Created by raj on 03/09/14.
+ */
+public class MapReduceTest {
+
+    private MongoClient mongoDBClient = MongoDBClient.getConnection();
+
+    @BeforeClass
+    public void init() {
+        DBCollection collection = mongoDBClient.getDB("test").getCollection("tradeCache");
+        for (BasicDBObject basicDBObject : TradeLoader.trades()) {
+            collection.insert(basicDBObject);
+        }
+
+    }
+
+
+    private String map() {
+        return new String("function() { emit(this.sourceBook, this.amount); };");
+    }
+
+    private String reduce() {
+        return new String("function(sourceBook, amount) { return Array.sum(amount); };");
+    }
+
+    @Test
+    public void runMapReduceOnCollectionSumAmountBySourceBook() {
+
+        DBCollection collection = mongoDBClient.getDB("test").getCollection("tradeCache");
+        MapReduceCommand cmd = new MapReduceCommand(collection, map(), reduce(), null, MapReduceCommand.OutputType.INLINE, null);
+        MapReduceOutput out = collection.mapReduce(cmd);
+
+        for (DBObject o : out.results()) {
+            System.out.println(o.toString());
+
+        }
+    }
+
+}
